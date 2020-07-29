815083cddaf55a50e99f4c6c6aeaca0a391622e5
Jacob Viti <thevitij64@gmail.com>
2020-07-29 01:16:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,14 +9,13 @@ class Anagram
   def match(words)
     matched = []
     words.each do |word|
-      return @anagram
     letters = word.split(//)
     #return letters
     anagramLetters = @anagram.split(//)
     #return anagramLetters
     if letters.sort == anagramLetters.sort
       matchedword = letters.join()
-      matched << matchedword
+      matched << word
    end
  end
    matched
